56ab5d05e30612b0452a59ada2043f841d57b0a4
Nicolas Renaud <nicolasrenaud@Nicolass-MacBook-Pro.local>
2012-11-06 13:54:36 +0100

    Initial Commit

---
+++ SpeakEasy/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SpeakEasy
+//
+//  Created by Nicolas Renaud on 11/6/12.
+//  Copyright (c) 2012 Nicolas Renaud. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
